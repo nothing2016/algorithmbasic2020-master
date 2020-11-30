ca6bbc978e9f4c1e4ae7d4c2e8a7f2c87ee614c9
oudaming <805269294@qq.com>
2020-11-30 23:37:33 +0800

    栈逆序的解释

--- src/class11/Code04_ReverseStackUsingRecursive.java
+++ src/class11/Code04_ReverseStackUsingRecursive.java
@@ -2,17 +2,33 @@ package class11;
 
 import java.util.Stack;
 
+/**
+ * 题目：如果将一个栈的内容逆序，但不能申请额外的数据结构空间
+ * 只可以使用少数几个变量
+ */
 public class Code04_ReverseStackUsingRecursive {
 
+	/**
+	 *
+	 * @param stack
+	 */
 	public static void reverse(Stack<Integer> stack) {
 		if (stack.isEmpty()) {
 			return;
 		}
 		int i = f(stack);
 		reverse(stack);
+		// 这一行一定是原来栈顶的值先被压到栈中的，这里有深度遍历的思维
+		// 只有深度到底了，然后才会return, 因为f(stack)函数是将一个栈的栈底值返回，栈上面的值往下沉
+		// 这样return的时候，一定是原来栈顶的元素最后return
 		stack.push(i);
 	}
 
+	/**
+	 * 将一个栈的栈底值返回，栈上面的值往下沉
+	 * @param stack
+	 * @return
+	 */
 	public static int f(Stack<Integer> stack) {
 		int result = stack.pop();
 		if (stack.isEmpty()) {
